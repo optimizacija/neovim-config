333c3ac1a01d6e35bb5fa572cf9f007cd88812ab
optimizacija <males.jan2@gmail.com>
2021-04-21 09:42:10 +0200

    installed treesitter and bug fixes

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -1,4 +1,4 @@
-opts = {silent = true, noremap = true}
+local opts = {silent = true, noremap = true}
 vim.api.nvim_set_keymap('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>r', '<Cmd>NvimTreeRefresh<CR>', opts)
 -- find the currently open file in tree
