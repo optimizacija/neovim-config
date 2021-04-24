285e6b9a2823872949e844b70f0f45200915d68d
optimizacija <males.jan2@gmail.com>
2021-04-24 10:37:59 +0200

    changed autocomplete behavior on enter

--- lua/basics.lua
+++ lua/basics.lua
@@ -82,3 +82,9 @@ vim.wo.wrap = false -- don't wrap long text into multiple lines
 vim.o.history = 10000 -- numbers of entries in history for ':' commands and search patterns (10000 = max)
 vim.o.updatetime = 300 -- used for CursorHold event (for document highlighting detection)
 vim.o.mouse = 'nv' -- allow mose in normal & visual mode
+
+-- better autocomplete behaviour
+-- menuone - show popup menu also when there is only one match available
+-- preview - show extra information about currently selected completion
+-- noinsert - do not insert any text for match until the user selects it from the menu
+vim.o.completeopt='menuone,preview,noinsert'
