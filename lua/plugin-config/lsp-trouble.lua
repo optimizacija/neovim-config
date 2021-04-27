bb9dd7b31d67187c7bc1e32b61945e3d3f5fe78d
Jan M <males.jan2@gmail.com>
2021-04-27 22:05:35 +0200

    added lsp diagnostics window & coloring

---
+++ lua/plugin-config/lsp-trouble.lua
@@ -0,0 +1,37 @@
+local opts = {silent = true, noremap = true}
+vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>LspTroubleToggle<cr>", opts)
+require('trouble').setup{
+    icons = true, -- use dev-icons for filenames
+    mode = "document", -- "workspace" or "document"
+    -- fold_open = "", -- icon used for open folds
+    -- fold_closed = "", -- icon used for closed folds
+    action_keys = { -- key mappings for actions in the trouble list
+        close = "q", -- close the list
+        cancel = {"ć", "Ć"}, -- cancel the preview and get back to your last window / buffer / cursor
+        refresh = "r", -- manually refresh
+        jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
+        toggle_mode = "m", -- toggle between "workspace" and "document" mode
+        toggle_preview = "P", -- toggle auto_preview
+        preview = "p", -- preview the diagnostic location
+        close_folds = {"zM", "zm"}, -- close all folds
+        open_folds = {"zR", "zr"}, -- open all folds
+        toggle_fold = {"zA", "za"}, -- toggle fold of current file
+        previous = "l", -- up
+        next = "k" -- down
+    },
+    indent_lines = true, -- add an indent guide below the fold icons
+    auto_open = true, -- automatically open the list when you have diagnostics
+    auto_close = true, -- automatically close the list when you have no diagnostics
+    auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back
+    auto_fold = false, -- automatically fold a file trouble list at creation
+		--[[
+    signs = {
+        -- icons / text used for a diagnostic
+        error = "",
+        warning = "",
+        hint = "",
+        information = ""
+    },
+		--]]
+    use_lsp_diagnostic_signs = true -- enabling this will use the signs defined in your lsp client
+}
