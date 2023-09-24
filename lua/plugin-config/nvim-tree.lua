4a9cdd537b31294bcdc83f660277495c7eea2af6
optimizacija <males.jan2@gmail.com>
2023-09-24 21:05:15 +0200

    feat: removed deprecated config for nvim-tree

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -59,12 +59,12 @@ require'nvim-tree'.setup {
   },
   view = {
     width = 30,
-    hide_root_folder = false,
+    -- hide_root_folder = false,
     side = 'left',
-    mappings = {
-      custom_only = false,
-      list = list,
-    }
+    --mappings = {
+      --custom_only = false,
+      --list = list,
+    --}
   },
   on_attach = my_on_attach
 }
