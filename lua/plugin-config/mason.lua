7d90dd58fdfab9a627ba9b322a52349b4429ce6b
optimizacija <males.jan2@gmail.com>
2023-09-25 09:54:57 +0200

    feat: setup other default lsps

--- lua/plugin-config/mason.lua
+++ lua/plugin-config/mason.lua
@@ -9,9 +9,16 @@ require("mason-lspconfig").setup {
         'docker_compose_language_service',
         'jsonls',
         'marksman',
-        'vimls'
+        'yamlls'
     },
 }
 
 require'lspconfig'.lua_ls.setup{}
 require'lspconfig'.rust_analyzer.setup{}
+require'lspconfig'.bashls.setup{}
+require'lspconfig'.taplo.setup{}
+require'lspconfig'.dockerls.setup{}
+require'lspconfig'.docker_compose_language_service.setup{}
+require'lspconfig'.jsonls.setup{}
+require'lspconfig'.marksman.setup{}
+require'lspconfig'.yamlls.setup{}
