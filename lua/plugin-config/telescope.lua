17072b2271c3ae68391058fad200597036fe160d
optimizacija <males.jan2@gmail.com>
2023-10-27 15:58:50 +0200

    cleanup: telescope

--- lua/plugin-config/telescope.lua
+++ lua/plugin-config/telescope.lua
@@ -6,43 +6,39 @@ vim.api.nvim_set_keymap('n', '<Leader>fd', "<Cmd>lua require'telescope.builtin'.
 -- this makes it possible to use flags such as "-P" which enables lookaround features, for example: (?<\w)
 -- -- only if your local installation of ripgrep allows the "-P" extension
 vim.api.nvim_set_keymap('n', '<leader>fa', "<Cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", opts)
--- vim.api.nvim_set_keymap('n', '<Leader>fb', "<Cmd>lua require'telescope.builtin'.buffers()<CR>", opts) -- unused
--- vim.api.nvim_set_keymap('n', '<Leader>fh', "<Cmd>lua require'telescope.builtin'.help_tags()<CR>", opts) -- unused
 
 local actions = require('telescope.actions')
 require('telescope').setup {
     defaults = {
         -- program to use for searching with its arguments
         find_command = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
-        -- prompt_position = 'top', -- have prompt at the top (has no effect on vertical configuration)
         prompt_prefix = ' ', -- symbol on prompt window
         selection_caret = ' ', -- symbol on selected row in results window
         entry_prefix = '  ', -- symbol on non-selected rows in results window
         initial_mode = 'insert', -- start in insert mode
         selection_strategy = 'reset', -- what happens to selection when list changes
         sorting_strategy = 'ascending', -- start with most important search on top
-        layout_strategy = 'vertical', -- vertical layout
+        layout_strategy = 'vertical',
         layout_config = {
             vertical = {
+                width = 0.9,
+                height = 0.9,
                 mirror = true, -- windows should be in this order from top to bottom: search, results, preview
-                preview_height = 0.5 -- preview window takes 0.5 of the total window height
+                prompt_position = 'top',
+                preview_cutoff = 0,
             }
         },
+        --preview = {
+            --filesize_limit = 3, -- disable preview for files bigger than specified number of MB
+        --},
+        path_display = {'tail'},
         file_sorter = require'telescope.sorters'.get_fuzzy_file,
         file_ignore_patterns = {'node_modules/.*'}, -- never search in node_modules/ dir
         generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
         display_path = true,
         winblend = 0, -- window should not be transparent
-        border = {}, -- no border?
-        borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'}, -- border chars
         color_devicons = true, -- colorize used icons
-        use_less = true, -- less is bash program for preview file contents
         set_env = {['COLORTERM'] = 'truecolor'}, -- use all the colors
-        file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-        grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-        qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
-        -- preview_cutoff = 120,
         mappings = {
             i = {
                 ['<C-k>'] = actions.move_selection_next,
@@ -59,10 +55,4 @@ require('telescope').setup {
             }
         }
     },
-    extensions = {
-        fzy_native = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
-        }
-    }
 }
